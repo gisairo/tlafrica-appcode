310785d5f1b66dc5c5ac89e8531802209c6865c6
James Brian <jamesgsr@gmail.com>
2015-02-27 05:26:05 +0300

    added login, registration, reset, intro styling and interface code

---
+++ app.js
@@ -0,0 +1,7 @@
+//js to load when device is ready e.g allow top ios app to be visible
+$( document ).on( "deviceready", function(){
+	jQuery(document).ready(function($) {
+		console.log("ready");
+	});
+
+});
